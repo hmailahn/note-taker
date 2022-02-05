aba69a0e9bd765c5e444673c62755980f33bbb66
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-02-05 16:25:14 -0600

    '

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const { notes } = require('../../data/notes.json');
 
+
+//this doesn't work
 router.get('/api/notes', (req, res) => {
-let results = notes;
-res.json(results);
+    res.json(notes);
 });
 
-router.post('/api/notes', (req, res) => {
-    
-})
+// router.post('/api/notes', (req, res) => {
+
+// })
 
 module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,10 +1,16 @@
 const path = require('path');
 const router = require('express').Router();
 
+//works
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
 
-
+//this works
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
 
 module.exports = router;
+
+//this file is good to go
