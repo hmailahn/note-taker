f18c6db6d587fb418397cd54dce03fd2d9182d9e
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-02-05 18:33:13 -0600

    got the get function to work in the apiRoutes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 const { notes } = require('../../data/notes.json');
+var uniqid = require('uniqid');
 
 
-//this doesn't work
-router.get('/api/notes', (req, res) => {
-    res.json(notes);
+//works? needs to be tested when notes are added
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
 });
 
-// router.post('/api/notes', (req, res) => {
-
-// })
+router.post('/api/notes', (req, res) => {
+req.body.id = uniqid();
+})
 
 module.exports = router;
