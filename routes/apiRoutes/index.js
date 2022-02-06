f3bd6eab5264ec6d5139d33a86e5960f46e0c379
Heidi Mailahn <92544035+hmailahn@users.noreply.github.com>
2022-02-05 18:54:09 -0600

    can't figure out post just yet

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,16 +1,35 @@
 const router = require('express').Router();
 const { notes } = require('../../data/notes.json');
 var uniqid = require('uniqid');
-
+const fs = require('fs');
+const path = require('path');
 
 //works? needs to be tested when notes are added
 router.get('/notes', (req, res) => {
     let results = notes;
-    res.json(results);
+    res.send('hello');
 });
 
+
+//this doesn;t work////
 router.post('/api/notes', (req, res) => {
-req.body.id = uniqid();
-})
+console.log(req.body);
+res.json(req.body);
+
+
+    // req.body.id = uniqid();
+    // const note = createNewNote(req.body, notes);
+    // res.json(note);
+});
+
+// function createNewNote(body, notesArray) {
+//     const note = body;
+//     notesArray.push(note);
+//     fs.writeFileSync(
+//         path.join(__dirname, '../data/notes.json'),
+//         JSON.stringify({ notesArray }, null, 2)
+//     );
+//     return note;
+// }
 
 module.exports = router;
